ba632ed47ecbf36e3aa9bbfe7f36387def0ae358
João Henrique De Menezes <133153411+joaohmenezes@users.noreply.github.com>
2026-06-18 11:13:31 -0300

    Add files via upload

--- labPDS/freqz_m.m
+++ labPDS/freqz_m.m
@@ -1,17 +1,8 @@
 function [db, mag, pha, w] = freqz_m(b, a)
-    % [db, mag, pha, w] = freqz_m(b, a)
-    % Modified freqz function for digital filter design
-    % b: numerator coefficients
-    % a: denominator coefficients
-    % db: magnitude response in dB
-    % mag: absolute magnitude response
-    % pha: phase response in radians
-    % w: frequency vector (0 to pi)
-    
     [H, w] = freqz(b, a, 1000, 'whole');
     H = (H(1:501))';
     w = (w(1:501))';
     mag = abs(H);
-    db = 20*log10((mag + eps)/(max(mag)));
+    db = 20*log10((mag+eps)/(max(mag)));
     pha = angle(H);
 end
